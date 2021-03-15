901c1fc22c9f64090db1db7d5739e66c73907f7f
Vladimir <voronov.volodya2013@yandex.ru>
2021-03-15 22:19:20 +0700

    :sbake: обновлен старый пример и добавлен новый

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from sequential_mh.bpp_dsc.rectangle import Material, Blank, Kit
 
 def example_1():
     """Пример расроя № 1"""
-    material = Material('Сплав 1', 2.2, 1.)
     data = {
         'name': 'Реальный пример 1',
         'kit': [
@@ -24,6 +23,46 @@ def example_1():
         'H0': 23,
         'H1': 3,
     }
+    return data
+
+
+def example_2():
+    """Реальный пример раскрая № 2
+
+    Большинство фигур одинаковы, только одна отличается.
+    Прокат должен приоритетно проходить в одну сторону.
+    """
+    data = {
+        'name': 'Реальный пример 2',
+        'kit': [
+            (54, 160, 3, 1), (220, 86, 3, 1), (86, 220, 3, 1), (86, 220, 3, 1),
+            (220, 86, 3, 1), (220, 86, 3, 1), (220, 86, 3, 1), (220, 86, 3, 1)
+        ],
+        'L0': 100,
+        'W0': 180,
+        'H0': 28,
+        'H1': 3,
+    }
+    return data
+
+
+EXAMPLES = [
+    example_1,
+    example_2
+]
+
+
+def main(example):
+    material = Material('Сплав 1', 2.2, 1.)
+
+    if 0 <= example - 1 < len(EXAMPLES):
+        data = EXAMPLES[example]()
+    else:
+        raise ValueError(
+            'Некорректный номер примера. '
+            f'Доступны примеры с номерами от {1} до {len(EXAMPLES)}'
+        )
+
     kit = []
     for item in data['kit']:
         kit.append(Blank(*item, material=material))
@@ -34,7 +73,7 @@ def example_1():
     H_0 = data['H0']
     H_1 = data['H1']
     _, main_region, result, _, tailings = bpp_ts(
-        L_0, W_0, H_0, H_1, kit[3], hem=(0, 5), is_visualize=False
+        L_0, W_0, H_0, H_1, kit[3], hem=(0, 5), allowance=5, is_visualize=True
     )
 
     print(
@@ -48,9 +87,6 @@ def example_1():
     )
 
 
-def main():
-    pass
-
-
 if __name__ == '__main__':
-    example_1()
+    example = 1
+    main(example)
