c5b596da53c42c27ac1371c9801532c0b4799fd9
Vladimir <voronov.volodya2013@yandex.ru>
2021-03-19 13:22:34 +0700

    :snake: Добавлены вспомогательные функции

---
+++ sequential_mh/bpp_dsc/support.py
@@ -0,0 +1,56 @@
+"""Модуль вспомогательных функций"""
+
+
+from typing import Union
+
+
+Number = Union[int, float]
+Vec3 = tuple[Number, Number, Number]
+
+
+def is_subrectangle(src: Vec3, dst: Vec3, with_rotate: bool=True) -> bool:
+    """Проверка вложения прямугольника в другой прямоугольник.
+
+    :param src: стороны прямоугольника который необходимо вписать
+                в виде (длина, ширина, высота)
+    :type src: tuple[Number, Number, Number]
+    :param dst: стороны большого прямоугольника в виде (длина, ширинаб высота)
+    :type dst: tuple[Number, Number, Number]
+    :param with_rotate: флаг, разрешающий поворот вписываемого
+                        прямоугольника на 90 градусов, по умолчанию True
+    :type with_rotate: bool
+    :return: True, если прямоугольник можно вписать
+             и False в противном случае
+    :rtype: bool
+    """
+    if dst[-1] == src[-1]:
+        if with_rotate:
+            return (
+                max(src[:2]) <= max(dst[:2]) and min(src[:2]) <= min(dst[:2])
+            )
+        return src[0] <= dst[0] and src[1] <= dst[1]
+    return False
+
+
+def is_subrectangle_with_def(src: Vec3, dst: Vec3, rolldir: int,
+                             with_rotate: bool=True, extension: Number=1):
+    length, width, height = dst
+    new_height = src[-1]
+    if rolldir == 0:
+        # по горизонтали
+        width = deformation(width, height, new_height, extension)
+    else:
+        # по вертикали
+        length = deformation(length, height, new_height, extension)
+    return is_subrectangle(
+        src, (length, width, new_height), with_rotate=with_rotate
+    )
+
+
+def deformation(length: Number, height: Number, new_height: Number,
+                extension: Number=1) -> Number:
+    if height == new_height:
+        return length
+    if height < new_height:
+        extension = 1 / extension
+    return (extension * length * height) / new_height
