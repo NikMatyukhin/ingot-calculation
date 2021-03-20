3411e7c9031081250e15d6633d774ba6ecf026fb
Vladimir <voronov.volodya2013@yandex.ru>
2021-03-20 19:11:40 +0700

    :art: Обновлены примеры демонстрации помедения кромок

--- research.py
+++ research.py
@@ -1,33 +1,34 @@
+"""Модуль для демонтрации различных аспектов раскроя
+
+:Date: 19.03.2020
+:Version: 0.2
+:Authors:
+    - Воронов Владимир Сергеевич
+"""
+
+import math
+
 import matplotlib.pyplot as plt
 
 
-def size_dependencies():
+def size_dependencies(width, length, height, g_height):
     """Область допустимых размеров листа"""
-    W_0 = 100
-    L_0 = 160
-    H_0 = 10
-    H_1 = 3
 
-    x = []
-    i = 100
-    while i < W_0*H_0 / H_1:
-        x.append(i)
-        i += 0.1
-
-    y_1 = [L_0 * W_0 * H_0 / (H_1 * i) for i in x]
-    y_2 = [j - 15 for j in y_1]
+    max_w = width * height / g_height
+    steps = math.ceil(max_w / 0.1)
+    x = [item for i in range(steps) if (item := width + i * 0.1) < max_w]
+    y_1 = [length * width * height / (g_height * i) for i in x]
 
     _, axis = plt.subplots()
     axis.plot(x, y_1, color='b', label='Максимальная граница размеров')
-    axis.hlines(y_1[0], 0, W_0, color='b')
+    axis.hlines(y_1[0], 0, width, color='b')
     axis.hlines(0, 0, x[-1], color='b')
-    axis.vlines(x[-1], 0, L_0, color='b')
+    axis.vlines(x[-1], 0, length, color='b')
     axis.vlines(0, 0, y_1[0], color='b')
-    axis.plot(x, y_2, color='g', label='Верхняя кромка')
     axis.legend()
     axis.grid()
 
-    plt.show()
+    return x, y_1, axis
 
 
 def hem_and_end():
@@ -38,20 +39,13 @@ def hem_and_end():
     top_hem = 15  # верхняя кромка/торец
     right_hem = 5  # правая кромка
 
-    x = []
-    i = W_0
-    while i < W_0*H_0 / H_1:
-        x.append(i)
-        i += 0.1
+    x, y_1, axis = size_dependencies(W_0, L_0, H_0, H_1)
 
-    y = []
-    i = L_0
-    while i < L_0*H_0 / H_1:
-        y.append(i)
-        i += 0.1
+    max_l = L_0 * H_0 / H_1
+    steps = math.ceil(max_l / 0.1)
+    y = [item for i in range(steps) if (item := L_0 + i * 0.1) < max_l]
 
     # исходные границы
-    y_1 = [L_0 * W_0 * H_0 / (H_1 * i) for i in x]
     x_1 = [L_0 * W_0 * H_0 / (H_1 * i) for i in y]
 
     # верхняя и правая кромки
@@ -59,8 +53,8 @@ def hem_and_end():
     x_2 = [i - right_hem for i in x_1]
 
     # варианты расположения точек
-    # point = (110, 250)  # точка внутри области
-    point = (202, 255)  # точка в области кромки/торца
+    point = (110, 250)  # точка внутри области
+    # point = (202, 255)  # точка в области кромки/торца
     # point = (500, 500)  # точка вне области
 
     # исходные оценки
@@ -74,19 +68,13 @@ def hem_and_end():
     y_est_right_hem = L_0 * W_0 * H_0 / (H_1 * (point[0] + right_hem)) - point[1]
     min_x = min(x_est_right_hem, x_est_top_hem)
     min_y = min(y_est_right_hem, y_est_top_hem)
-    print(min_x, min_y)
+    print(f'Оценка с учетом кромок: {min_x, min_y}')
+    print(f'Оценка кромок: {x_est - min_x, y_est - min_y}')
 
-    _, axis = plt.subplots()
-    axis.plot(x, y_1, color='b', label='Максимальная граница размеров')
-    axis.hlines(y_1[0], 0, W_0, color='b')
-    axis.hlines(0, 0, x[-1], color='b')
-    axis.vlines(x[-1], 0, L_0, color='b')
-    axis.vlines(0, 0, y_1[0], color='b')
     axis.plot(x, y_2, color='g', label='Верхняя кромка')
     axis.plot(x_2, y, color='r', label='Правая кромка')
-    # axis.plot(x_1, y, color='g', label='Максимальная граница размеров')
 
-    axis.plot(*point, marker='o')
+    axis.plot(*point, marker='o', color='r', label='Точка оценки')
     axis.plot(point[0] + x_est_right_hem, point[1], marker='o', color='orange')
     axis.plot(point[0], point[1] + y_est_top_hem, marker='o', color='plum')
     axis.plot(point[0] + x_est_top_hem, point[1], marker='o', color='orange')
@@ -100,11 +88,47 @@ def hem_and_end():
     )
 
     axis.legend()
-    axis.grid()
+    plt.show()
 
+
+def hem_and_end_2():
+    # изначальные размеры
+    W_0, L_0, H_0 = 100, 160, 10
+    H_1 = 3
+    top_hem = 15  # верхняя кромка/торец
+    right_hem = 5  # правая кромка
+
+    # варианты расположения точек
+    point = (110, 250)  # точка внутри области
+    # point = (202, 255)  # точка в области кромки/торца
+    # point = (500, 500)  # точка вне области
+
+    x, _, axis = size_dependencies(W_0, L_0, H_0, H_1)
+
+    border = [L_0 * W_0 * H_0 / (H_1 * (i + right_hem)) - top_hem for i in x]
+
+    _y_est = L_0 * W_0 * H_0 / (H_1 * (point[0] + right_hem)) - top_hem - point[1]
+    _x_est = L_0 * W_0 * H_0 / (H_1 * (point[1] + top_hem)) - right_hem - point[0]
+    y_est = L_0 * W_0 * H_0 / (H_1 * point[0]) - point[1]
+    x_est = L_0 * W_0 * H_0 / (H_1 * point[1]) - point[0]
+    print(f'Оценка двойной кромки: {_x_est, _y_est}')
+    print(f'Размер кромок: {x_est - _x_est, y_est - _y_est}')
+
+    axis.plot(x, border, label='Граница кромок/торцов')
+    axis.vlines(
+        point[0], point[1], point[1] + _y_est, linestyle='--', color='k'
+    )
+    axis.hlines(
+        point[1], point[0], point[0] + _x_est, linestyle='--', color='k'
+    )
+    axis.plot(*point, marker='o', color='r', label='Точка оценки')
+    axis.plot(point[0], point[1] + _y_est, marker='o', color='k')
+    axis.plot(point[0] + _x_est, point[1], marker='o', color='k')
+
+    axis.legend()
     plt.show()
 
 
 if __name__ == '__main__':
-    # size_dependencies()
-    hem_and_end()
+    # hem_and_end()
+    hem_and_end_2()
