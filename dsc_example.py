433ee01814a837e46d77db778c6baed5be66dd4d
Vladimir <voronov.volodya2013@yandex.ru>
2021-03-20 19:14:51 +0700

    :memo: Обновлен пример работы с алгоритмом, добавлена визуализация карт

--- dsc_example.py
+++ dsc_example.py
@@ -1,9 +1,18 @@
+"""Пример работы с древовидной метаэвристикой"""
+
 import os
+from itertools import chain
 
 from sequential_mh.bpp_dsc.rectangle import Material, Blank, Kit, Bin, BinType
-from sequential_mh.bpp_dsc.tree import BinNode, Tree, optimal_configuration, solution_efficiency
+from sequential_mh.bpp_dsc.tree import (
+    BinNode, Tree, optimal_configuration, solution_efficiency
+)
 from sequential_mh.bpp_dsc.stm import _stmh_idrd
 from sequential_mh.bpp_dsc.graph import plot, create_edges
+
+from sequential_mh.tsh import rect
+from sequential_mh.tsh.est import Estimator
+from sequential_mh.tsh.visualize import visualize
 
 
 os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin'
@@ -32,6 +41,8 @@ def main(use_graphviz=False):
         'cutting_length': 1200,  # максимальная длина реза
         'hem_until_3': 10,  # кромка > 3 мм
         'hem_after_3': 5,  # кромка <= 3 мм
+        'allowance': 5,  # припуски на разрез
+        'end': 0.02,  # торцы в процентах от длины
     }
     kit = []
     for item in data:
@@ -51,10 +62,22 @@ def main(use_graphviz=False):
     efficiency, res, nodes = optimal_configuration(tree, nd=False)
     res.update_size()
 
+    for node in res.cc_leaves:
+        # node.result.blanks.values
+        main_rect = rect.Rectangle.create_by_size(
+            (0, 0), node.bin.length, node.bin.width
+        )
+        main_region = Estimator(main_rect, node.bin.height, node.bin.height)
+        rectangles = chain.from_iterable(node.result.blanks.values())
+        visualize(
+            main_region, rectangles, node.result.tailings,
+            xlim=node.bin.width + 50, ylim=node.bin.length + 50
+        )
+
     print(f'Всего деталей: {res.kit.qty()}')
     print(f'По всему объему: {solution_efficiency(res, nodes, is_total=True)}')
     print(f'По используемому объему: {solution_efficiency(res, nodes)}')
-    print(f'Взвешанная: {solution_efficiency(res, nodes, nd=True)}')
+    print(f'Взвешенная: {solution_efficiency(res, nodes, nd=True)}')
 
     if use_graphviz:
         graph1, all_nodes1 = plot(res, 'pdf/graph3.gv')
