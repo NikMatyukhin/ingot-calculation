f04175368e8d1c2d405fc1b6c00bd400eabb23b6
Nikita <nmatryukhin@mail.ru>
2021-04-01 22:35:28 +0700

    :snake: Добавлен полноэкранный режим

---
+++ gui/ui_full_screen.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'full_screenTsdaGj.ui'
+##
+## Created by: Qt User Interface Compiler version 6.0.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+
+import application_rc
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
+        Dialog.resize(640, 529)
+        self.verticalLayout = QVBoxLayout(Dialog)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.graphicsView = QGraphicsView(Dialog)
+        self.graphicsView.setObjectName(u"graphicsView")
+
+        self.verticalLayout.addWidget(self.graphicsView)
+
+        self.frame = QFrame(Dialog)
+        self.frame.setObjectName(u"frame")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout = QHBoxLayout(self.frame)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.label = QLabel(self.frame)
+        self.label.setObjectName(u"label")
+        self.label.setMaximumSize(QSize(40, 40))
+        self.label.setPixmap(QPixmap(u":/icons/scissors.png"))
+        self.label.setScaledContents(True)
+
+        self.horizontalLayout.addWidget(self.label)
+
+        self.label_2 = QLabel(self.frame)
+        self.label_2.setObjectName(u"label_2")
+
+        self.horizontalLayout.addWidget(self.label_2)
+
+        self.label_3 = QLabel(self.frame)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setMaximumSize(QSize(40, 40))
+        self.label_3.setPixmap(QPixmap(u":/icons/roll.png"))
+        self.label_3.setScaledContents(True)
+
+        self.horizontalLayout.addWidget(self.label_3)
+
+        self.label_4 = QLabel(self.frame)
+        self.label_4.setObjectName(u"label_4")
+
+        self.horizontalLayout.addWidget(self.label_4)
+
+        self.label_5 = QLabel(self.frame)
+        self.label_5.setObjectName(u"label_5")
+        self.label_5.setMinimumSize(QSize(45, 0))
+        self.label_5.setMaximumSize(QSize(45, 40))
+        self.label_5.setPixmap(QPixmap(u":/icons/blueprint.png"))
+        self.label_5.setScaledContents(True)
+
+        self.horizontalLayout.addWidget(self.label_5)
+
+        self.label_6 = QLabel(self.frame)
+        self.label_6.setObjectName(u"label_6")
+
+        self.horizontalLayout.addWidget(self.label_6)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer)
+
+
+        self.verticalLayout.addWidget(self.frame)
+
+
+        self.retranslateUi(Dialog)
+
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"\u041a\u0430\u0440\u0442\u0430: \u043f\u043e\u043b\u043d\u043e\u044d\u043a\u0440\u0430\u043d\u043d\u044b\u0439 \u0440\u0435\u0436\u0438\u043c", None))
+        self.label.setText("")
+        self.label_2.setText(QCoreApplication.translate("Dialog", u"\u041e\u043f\u0435\u0440\u0430\u0446\u0438\u044f\n"
+"\u0440\u0430\u0441\u043a\u0440\u043e\u044f", None))
+        self.label_3.setText("")
+        self.label_4.setText(QCoreApplication.translate("Dialog", u"\u041e\u043f\u0435\u0440\u0430\u0446\u0438\u044f\n"
+"\u043f\u0440\u043e\u043a\u0430\u0442\u0430", None))
+        self.label_5.setText("")
+        self.label_6.setText(QCoreApplication.translate("Dialog", u"\u041e\u043f\u0435\u0440\u0430\u0446\u0438\u044f\n"
+"\u0443\u043f\u0430\u043a\u043e\u0432\u043a\u0438", None))
+    # retranslateUi
+
