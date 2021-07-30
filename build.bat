a3113ccba3c3b1089894027aad32be3d78df02c7
Nikita <nmatryukhin@mail.ru>
2021-07-30 09:16:31 +0700

    :gear: Добавлен перенос настроек при сборке

--- build.bat
+++ build.bat
@@ -2,4 +2,5 @@ call venv\Scripts\activate.bat
 pyinstaller mainwindow.spec
 mkdir dist\mainwindow\schemes
 break > dist\mainwindow\log\info.log
+copy config dist\mainwindow
 call venv\Scripts\deactivate
